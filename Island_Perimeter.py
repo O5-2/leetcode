d9dea58cf7335ed2557e4d40ad9a56bb51f0adbb
Elijah Karp <eli@Elijahs-MacBook-Air.local>
2017-02-16 11:05:06 -0800

    Added code for Island Perimeter

---
+++ Island_Perimeter.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def islandPerimeter(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        perimeter = 0
+        for i in range(0, len(grid)):
+            for j in range(0, len(grid[i])):
+                if grid[i][j] == 1:
+                    if i != 0:
+                        if grid[i-1][j] == 0:
+                            perimeter += 1
+                    else:
+                        perimeter += 1
+                    if j != 0:
+                        if grid[i][j-1] == 0:
+                            perimeter += 1
+                    else:
+                        perimeter += 1
+                    if i != len(grid)-1:
+                        if grid[i+1][j] == 0:
+                            perimeter += 1
+                    else:
+                        perimeter += 1
+                    if j != len(grid[i])-1:
+                        if grid[i][j+1] == 0:
+                            perimeter += 1
+                    else:
+                        perimeter += 1
+        return perimeter

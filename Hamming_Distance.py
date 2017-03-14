0f44de16867ae6a1aaa778d4b9269d8634152a45
Elijah Karp <eli@Elijahs-MacBook-Air.local>
2017-03-14 10:30:01 -0700

    Added code for Hamming Distance

---
+++ Hamming_Distance.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        ans = 0
+        while x != 0 or y != 0:
+            if x%2 != y%2:
+                ans += 1
+            x = x >> 1
+            y = y >> 1
+        return ans
